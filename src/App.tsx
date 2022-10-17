df889d42bb5c7351917e490d66a47e5f6eac2808
Enos Domingues <enosdomingues@gmail.com>
2022-10-17 22:32:27 +0200

    after

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import './styles/global.css'
+import { Logo } from './Logo'
+import { Heading } from './components/Heading'
+import { Text } from './components/Text'
+import { TextInput } from './components/TextInput'
+import { Envelope, Lock } from 'phosphor-react'
+import { Checkbox } from './components/Checkbox'
+import { Button } from './components/Button'
+import { SignIn } from './pages/SignIn'
 
 export function App() {
   return (
-    <>
-      <h1 className="font-bold text-2xl text-violet-500">Hello World</h1>
-
-      <button className="bg-cyan-500 font-medium px-4 py-2 rounded text-white hover:bg-cyan-300">
-        Enviar
-      </button>
-    </>
+    <SignIn />
   )
 }
